9d248be87f7bb3158ba91c22398a041fb86f1067
DMG <daniel.marce@mataro.epiaedu.cat>
2024-10-28 19:30:13 +0100

    fix: remove tabulation

--- js/index.js
+++ js/index.js
@@ -1,56 +1,56 @@
-    const createNote = (event) => {
-        const titleValue = event.target.title.value;
-        const contentValue = event.target.content.value;
-        let priorityValue = event.target.priority.value;
+const createNote = (event) => {
+    const titleValue = event.target.title.value;
+    const contentValue = event.target.content.value;
+    let priorityValue = event.target.priority.value;
 
-        if (priorityValue == 0) {
-            priorityValue = "No priority";
-        }
-
-        const $unorderedList = window.document.querySelector('.section__list');
-
-        const $listItemWrapped = window.document.createElement('div');
-        $listItemWrapped.classList.add('listItem__wrapped');
-        
-        const $listItem = window.document.createElement('li');
-        $listItem.classList.add('list__item');
-
-        const $headingItem = window.document.createElement('h3');
-        $headingItem.classList.add('item__title');
-        const $textTitle = window.document.createTextNode(titleValue);
-        $headingItem.appendChild($textTitle);
-
-        const $contentParagraphItem = window.document.createElement('p');
-        $contentParagraphItem.classList.add('item__content');
-        const $textContent = window.document.createTextNode(contentValue);
-        $contentParagraphItem.appendChild($textContent);
-
-        const $priorityParagraphItem = window.document.createElement('p');
-        $priorityParagraphItem.classList.add('item__priority');
-        const $textPriority = window.document.createTextNode(priorityValue);
-        $priorityParagraphItem.appendChild($textPriority);
-
-        $listItem.appendChild($headingItem);
-        $listItem.appendChild($contentParagraphItem);
-        $listItem.appendChild($priorityParagraphItem);
-
-        $listItemWrapped.appendChild($listItem)    
-
-        $unorderedList.appendChild($listItemWrapped);
-
-        const $inputTitle = window.document.querySelector('#title');
-        const $inputContent = window.document.querySelector('#content');
-        const $inputPriority = window.document.querySelector('#priority');
-
-        $inputTitle.value = "";
-        $inputContent.value = "";
-        $inputPriority.value = 0;
+    if (priorityValue == 0) {
+        priorityValue = "No priority";
     }
 
-    const $form = window.document.querySelector('.section__form');
+    const $unorderedList = window.document.querySelector('.section__list');
 
-    $form.addEventListener('submit', (event) => {
-        event.preventDefault();
+    const $listItemWrapped = window.document.createElement('div');
+    $listItemWrapped.classList.add('listItem__wrapped');
+    
+    const $listItem = window.document.createElement('li');
+    $listItem.classList.add('list__item');
 
-        createNote(event);
-    })
+    const $headingItem = window.document.createElement('h3');
+    $headingItem.classList.add('item__title');
+    const $textTitle = window.document.createTextNode(titleValue);
+    $headingItem.appendChild($textTitle);
+
+    const $contentParagraphItem = window.document.createElement('p');
+    $contentParagraphItem.classList.add('item__content');
+    const $textContent = window.document.createTextNode(contentValue);
+    $contentParagraphItem.appendChild($textContent);
+
+    const $priorityParagraphItem = window.document.createElement('p');
+    $priorityParagraphItem.classList.add('item__priority');
+    const $textPriority = window.document.createTextNode(priorityValue);
+    $priorityParagraphItem.appendChild($textPriority);
+
+    $listItem.appendChild($headingItem);
+    $listItem.appendChild($contentParagraphItem);
+    $listItem.appendChild($priorityParagraphItem);
+
+    $listItemWrapped.appendChild($listItem)    
+
+    $unorderedList.appendChild($listItemWrapped);
+
+    const $inputTitle = window.document.querySelector('#title');
+    const $inputContent = window.document.querySelector('#content');
+    const $inputPriority = window.document.querySelector('#priority');
+
+    $inputTitle.value = "";
+    $inputContent.value = "";
+    $inputPriority.value = 0;
+}
+
+const $form = window.document.querySelector('.section__form');
+
+$form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    createNote(event);
+})
