24ddabd24dfc461e336b720ea415d6d0868195f1
DMG <daniel.marce@mataro.epiaedu.cat>
2024-10-31 18:45:14 +0100

    fix: improve text nodes naming and implement a more direct way to reset form values

--- js/index.js
+++ js/index.js
@@ -14,32 +14,24 @@ const createNote = (event) => {
 
     const $headingItem = window.document.createElement('h3');
     $headingItem.classList.add('item__title');
-    const $textTitle = window.document.createTextNode(titleValue);
-    $headingItem.appendChild($textTitle);
+    const $headingTitle = window.document.createTextNode(titleValue);
+    $headingItem.appendChild($headingTitle);
 
     const $contentParagraphItem = window.document.createElement('p');
     $contentParagraphItem.classList.add('item__content');
-    const $textContent = window.document.createTextNode(contentValue);
-    $contentParagraphItem.appendChild($textContent);
+    const $paragraphContent = window.document.createTextNode(contentValue);
+    $contentParagraphItem.appendChild($paragraphContent);
 
     const $priorityParagraphItem = window.document.createElement('p');
     $priorityParagraphItem.classList.add('item__priority');
-    const $textPriority = window.document.createTextNode(priorityValue);
-    $priorityParagraphItem.appendChild($textPriority);
+    const $paragraphPriorityItem = window.document.createTextNode(priorityValue);
+    $priorityParagraphItem.appendChild($paragraphPriorityItem);
 
     $listItem.appendChild($headingItem);
     $listItem.appendChild($contentParagraphItem);
     $listItem.appendChild($priorityParagraphItem);  
 
     $unorderedList.appendChild($listItem);
-
-    const $inputTitle = window.document.querySelector('#title');
-    const $inputContent = window.document.querySelector('#content');
-    const $inputPriority = window.document.querySelector('#priority');
-
-    $inputTitle.value = "";
-    $inputContent.value = "";
-    $inputPriority.value = 0;
 }
 
 const $form = window.document.querySelector('.section__form');
@@ -48,4 +40,5 @@ $form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     createNote(event);
+    $form.reset();
 })
